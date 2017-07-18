8c1bb9e750b70b59ee505f10e70d83650f2299d6
JoshuaASC4 <joshua.a.forde@gmail.com>
2017-07-18 10:51:37 -0400

    mariopaint and pokeball

---
+++ Projects/joshua-mariopaint/sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(900,900);
+    background(225);
+}
+function mouseDragged(){
+    ellipse(mouseX, mouseY, 60, 5);
+    /* for (var i = 0; i < 100; i++) {
+     var r = random(50);
+     stroke(r*5);
+     line(50, i, 50+r, i);
+            
+        } */   
+    }
+ 
+    
+   
